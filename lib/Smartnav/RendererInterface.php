bdc5462c47ac5ba1e359c1ae313a77845ab6c447
Joe Green <joe.green0991@gmail.com>
2013-05-07 19:06:18 +0100

    added extra stuff

--- lib/Smartnav/RendererInterface.php
+++ lib/Smartnav/RendererInterface.php
@@ -1,5 +1,5 @@
 <?php namespace Smartnav;
 interface RendererInterface {
-    public function render(Menu $menu,$menuElements,$attributes);
+    public function render(Menu $menu,$menuElements,$attributes,$level);
     public function render_item(Menu $menu,array $items);
 }
