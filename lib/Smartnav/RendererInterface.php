cac178b92c970a944bcb6fcbf3766490b7d4e6ab
joegreen0991 <joe.green0991@gmail.com>
2013-08-01 10:48:24 +0100

    Update RendererInterface.php

--- lib/Smartnav/RendererInterface.php
+++ lib/Smartnav/RendererInterface.php
@@ -1,6 +1,6 @@
 <?php namespace Smartnav;
 interface RendererInterface {
-    public function render(Menu $menu,$menuElements,$attributes,$level);
-    public function render_top(Menu $menu,$menuElements,$attributes);
-    public function render_item(Menu $menu,array $items);
+    public function render(Menu $menu, $menuElements, $attributes, $level);
+    public function render_top(Menu $menu, $menuElements, $attributes);
+    public function render_item(Menu $menu, $items);
 }
