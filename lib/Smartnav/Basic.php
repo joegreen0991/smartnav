bea789f9e788eab9edc692d0c2686cba95482029
joegreen0991 <joe.green0991@gmail.com>
2013-08-01 09:50:13 +0100

    Update Basic.php

--- lib/Smartnav/Basic.php
+++ lib/Smartnav/Basic.php
@@ -1,15 +1,15 @@
 <?php namespace Smartnav;
 class Basic implements RendererInterface {
     
-    public function render(Menu $menu,$menuElements,$attributes){
+    public function render(Menu $menu, $menuElements, $attributes, $level){
            return $menu->element ? '<' . $menu->element . $menu->attr($attributes) . '>' . $menuElements . '</' . $menu->element . '>' : $menuElements;
     }
     
-    public function render_top(Menu $menu,$menuElements,$attributes){
+    public function render_top(Menu $menu, $menuElements, $attributes){
         return $menuElements;
     }
 
-    public function render_item(Menu $menu,array $item){
+    public function render_item(Menu $menu, array $item){
         $attrs = $item;
         foreach (array('name', 'pages', 'group', 'list_attributes') as $invalid) {
             if (isset($attrs[$invalid]))
