d83c5eee99ced5d9c3dc19e59e8722451188bc83
Joe Green <joe.green0991@gmail.com>
2014-04-01 09:49:34 +0100

    Create Bootstrap3Renderer.php

---
+++ lib/Smartnav/Bootstrap3Renderer.php
@@ -0,0 +1,34 @@
+<?php namespace Smartnav;
+Class Bootstrap3Renderer implements RendererInterface {
+
+  public function render(Menu $menu, $menuElements, $attributes, $level) {
+      
+                $level and $attributes['class'] = isset($attributes['class']) ? $attributes['class'] . ' dropdown-menu' : 'dropdown-menu';
+      
+		return $menu->element ? '<' . $menu->element . ($level ? ' class="dropdown-menu" ' : $menu->attr($attributes)) . '>' . $menuElements . '</' . $menu->element . '>' : $menuElements;
+	}
+
+	public function render_top(Menu $menu, $menuElements, $attributes) {
+		return $menuElements;
+	}
+
+	public function render_item(Menu $menu, $item) {
+		$attrs = $item;
+
+		$caret = empty($item['pages']) ? '' : '<i class="icon-chevron-down"></i>';
+
+		foreach (array('name', 'pages', 'group', 'list_attributes') as $invalid) {
+			if (isset($attrs[$invalid]))
+				unset($attrs[$invalid]);
+		}
+
+		$icon = empty($item['icon']) ? '' : '<i class="' . $item['icon'] . '"></i> ';
+
+                $attrs['class'] = isset($attrs['class']) ? $attrs['class'] . ' dropdown-toggle' : 'dropdown-toggle';
+                
+                $attrs['data-toggle'] = "dropdown";
+                
+		return '<li' . $menu->attr($item['list_attributes']) . '><a' . $menu->attr($attrs) . '>' . $icon . '<span>' . $item['name'] . '</span>' . $caret . '</a>' . $item['pages'] . '</li>';
+	}
+
+}
